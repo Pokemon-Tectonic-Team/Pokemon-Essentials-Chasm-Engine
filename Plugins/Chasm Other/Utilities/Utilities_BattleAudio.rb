d79eaf23a98bf15bbe92f9c2de8f6cd1db7f911c
ZinniaGameDev <67340902+xeuorux@users.noreply.github.com>
2026-01-10 13:36:04 -0700

    Update Utilities_BattleAudio.rb
    Replaced $PokemonGlobal.tarot_amulet_active with tarotAmuletActive?

--- Plugins/Chasm Other/Utilities/Utilities_BattleAudio.rb
+++ Plugins/Chasm Other/Utilities/Utilities_BattleAudio.rb
@@ -75,7 +75,7 @@ def pbGetTrainerBattleBGM(trainer) # can be a Player, NPCTrainer or an array of 
     trainerarray.each do |t|
         trainer_type_data = GameData::TrainerType.get(t.trainer_type)
         music = trainer_type_data.battle_BGM if trainer_type_data.battle_BGM
-        music = trainer_type_data.cursed_battle_BGM if $PokemonGlobal.tarot_amulet_active && trainer_type_data.cursed_battle_BGM
+        music = trainer_type_data.cursed_battle_BGM if tarotAmuletActive? && trainer_type_data.cursed_battle_BGM
     end
     ret = pbStringToAudioFile(music) if music && music != ""
     unless ret
@@ -97,7 +97,7 @@ def pbGetTrainerBattleBGMFromType(trainertype)
     return $PokemonGlobal.nextBattleBGM.clone if $PokemonGlobal.nextBattleBGM
     trainer_type_data = GameData::TrainerType.get(trainertype)
     ret = trainer_type_data.battle_BGM if trainer_type_data.battle_BGM
-    ret = trainer_type_data.cursed_battle_BGM if $PokemonGlobal.tarot_amulet_active && trainer_type_data.cursed_battle_BGM
+    ret = trainer_type_data.cursed_battle_BGM if tarotAmuletActive? && trainer_type_data.cursed_battle_BGM
     unless ret
         # Check map metadata
         map_metadata = GameData::MapMetadata.try_get($game_map.map_id)
