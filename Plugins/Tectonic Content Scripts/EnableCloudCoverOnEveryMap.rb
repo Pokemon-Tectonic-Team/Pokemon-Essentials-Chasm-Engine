50e5d519fc8a5233c1d90bfa93ffcdfbecfb35ff
Azeler3 <dleguen@outlook.fr>
2026-01-18 12:56:43 +0100

    Revert "Merge branch 'azeler-ability-pass' into azeler-ability-pass"
    This reverts commit cd5e411950f8a8f3949ed7576c54b3a21ae46f94, reversing
    changes made to 2620d8a179f41f3439f39291dbdcca519263a20e.

--- Plugins/Tectonic Content Scripts/EnableCloudCoverOnEveryMap.rb
+++
@@ -1,5 +0,0 @@
-# Enable the Cloud Cover graphics on any map
-# Can be disabled on individual maps by creating a Parallel Process event
-Events.onMapChange += proc { |_sender,_e|
-    setGlobalSwitch(AUTO_CLOUDS_DISABLED_GLOBAl,false)
-}
