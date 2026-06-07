9ad34cef224efd9d875ab842ebef4f9300a013cb
ZinniaGameDev <xeuorux@gmail.com>
2026-06-07 11:27:58 -0700

    EXP-EZ Dispenser rework v1
    When the player receives EXP Candy, it is no longer stored in their bag. Instead, an equivalent amount of EXP to what its worth is stored in the EXP-EZ Dispenser.
    The EXP-EZ Dispenser now have a different function when used. It is now used directly onto Pokemon, where it gives it as much EXP Candy as it can from its stores (without going over the level cap).
    The EXP-EZ Dispenser is now stored in the Candy pocket

--- Plugins/Chasm Other/Items/Item Behaviours/EXPEZDispenser.rb
+++ Plugins/Chasm Other/Items/Item Behaviours/EXPEZDispenser.rb
@@ -1,56 +1,83 @@
-EXP_PER_EXTRA_SMALL = 250
 EXP_JAR_BASE_EFFICIENCY = 1.0
 
 class PokemonGlobalMetadata
 	attr_accessor :expJARUpgraded
 end
 
-ItemHandlers::UseFromBag.add(:EXPEZDISPENSER,proc { |item|
-	next useEXPEZ
-})
+ItemHandlers::UseOnPokemon.add(:EXPEZDISPENSER,proc { |item,pkmn,scene|
+	current_lvl = pkmn.level
+	current_exp = pkmn.exp
+	level_cap = LEVEL_CAPS_USED ? getLevelCap : growth_rate.max_level
 
-
-ItemHandlers::ConfirmUseInField.add(:EXPEZDISPENSER,proc { |item|
-  next true
-})
-
-ItemHandlers::UseInField.add(:EXPEZDISPENSER,proc { |item|
-	next useEXPEZ
-})
-
-def useEXPEZ()
-	$PokemonGlobal.expJAR = 0 if $PokemonGlobal.expJAR.nil?
-	$PokemonGlobal.expJARUpgraded = false if $PokemonGlobal.expJARUpgraded.nil?
-	pbMessage(_INTL("\\i[EXPEZDISPENSER]You have {1} EXP stored in the EXP-EZ Dispenser.",separate_comma($PokemonGlobal.expJAR)))
-
-	xsCandyTotal, sCandyTotal, mCandyTotal, lCandyTotal = calculateCandySplitForEXP($PokemonGlobal.expJAR,$PokemonGlobal.expJARUpgraded)
-
-	# Prompt the player to make the candies
-	if xsCandyTotal > 0 || sCandyTotal > 0 || mCandyTotal > 0 || lCandyTotal > 0
-		if !$PokemonGlobal.expJARUpgraded
-			if pbConfirmMessage(_INTL("\\i[EXPEZDISPENSER]You can make {1} Medium, {2} Small and {3} Extra-Small candies. Would you like to?",
-					separate_comma(mCandyTotal), separate_comma(sCandyTotal), separate_comma(xsCandyTotal)))
-				pbReceiveItem(:EXPCANDYM,mCandyTotal) if mCandyTotal > 0
-				pbReceiveItem(:EXPCANDYS,sCandyTotal) if sCandyTotal > 0
-				pbReceiveItem(:EXPCANDYXS,xsCandyTotal) if xsCandyTotal > 0
-				$PokemonGlobal.expJAR = $PokemonGlobal.expJAR % EXP_PER_EXTRA_SMALL
-			end
-		else
-			if pbConfirmMessage(_INTL("\\i[EXPEZDISPENSER]You can make {1} Large, {2} Medium and {3} Small candies. Would you like to?",
-					separate_comma(lCandyTotal), separate_comma(mCandyTotal), separate_comma(sCandyTotal)))
-				pbReceiveItem(:EXPCANDYL,lCandyTotal) if lCandyTotal > 0
-				pbReceiveItem(:EXPCANDYM,mCandyTotal) if mCandyTotal > 0
-				pbReceiveItem(:EXPCANDYS,sCandyTotal) if sCandyTotal > 0
-				$PokemonGlobal.expJAR = $PokemonGlobal.expJAR % (EXP_PER_EXTRA_SMALL * 4)
-			end
-		end
-	else
-		pbMessage(_INTL("\\i[EXPEZDISPENSER]That's not enough to make any candies."))
-		return 0
+	# Do nothing if the EXP-EZ Dispenser is empty
+	if $PokemonGlobal.expJAR == 0
+		pbSceneDefaultDisplay(_INTL("There is no EXP stored!"),scene)
+		next false
 	end
-	return 1
-end
 
+	# Do nothing if the pokemon's already at the level cap
+	if pkmn.level >= level_cap
+		pbSceneDefaultDisplay(_INTL("It won't have any effect."),scene)
+		next false
+	end
+
+	# Max XP and level
+	maxxp = pkmn.growth_rate.minimum_exp_for_level(level_cap)
+	
+	expAmount = [maxxp - current_exp, $PokemonGlobal.expJAR].min
+
+	# Apply the new EXP, accounting for the level cap
+	$PokemonGlobal.expJAR -= expAmount
+	pkmn.exp += expAmount
+	new_level = pkmn.level
+	if new_level == level_cap
+		pbSceneDefaultDisplay(_INTL("{1} gained only {3} Exp. Points due to the level cap at level {2}.", pkmn.name, level_cap, separate_comma(expAmount)),scene)
+	else
+		pbSceneDefaultDisplay(_INTL("{1} gained {2} Exp. Points!", pkmn.name, separate_comma(expAmount)),scene)
+	end
+	scene&.pbRefresh
+
+	# Leave if didn't level up
+	next true if new_level == current_lvl
+
+	# Show messages surrounding leveling up
+	showPokemonChangesWindow(pkmn) do
+		pkmn.calc_stats
+		scene&.pbRefresh
+		pbMessage(_INTL("{1} grew to Lv. {2}!", pkmn.name, new_level))
+	end
+
+	(new_level - current_lvl).times do
+		pkmn.changeHappiness("candylevelup")
+	end
+	
+	# Learn new moves upon level up
+	unless $Options.prompt_level_moves == 1
+		movelist = pkmn.getMoveList
+		for i in movelist
+			next if i[0] <= current_lvl
+			break if i[0] > new_level
+			pbLearnMove(pkmn, i[1], true)
+		end
+	end
+
+	# Check for evolution
+	while true
+		newspecies = pkmn.check_evolution_on_level_up
+		break unless newspecies
+		evolutionSuccess = false
+		pbFadeOutInWithMusic do
+			evo = PokemonEvolutionScene.new
+			evo.pbStartScreen(pkmn, newspecies)
+			evolutionSuccess = true if evo.pbEvolution
+			evo.pbEndScreen
+			scene&.pbRefresh
+		end
+		break unless evolutionSuccess
+	end
+
+	next true
+})
 
 def calculateCandySplitForEXP(expAmount, biggerSized = false)
 	# Calculate how many of each candy size could be given
